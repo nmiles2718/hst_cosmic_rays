38c7730b3f05341c1f9bfb8352fb3aee2393d14d
Nathan Miles <ndmiles@Nathans-MacBook-Pro.local>
2020-09-21 08:49:31 -0700

    removing tangential CR analysis work

--- analyzing_cr_rejection/ocrreject.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-exptime1=$1
-exptime2=$2
-crsigmas=$3
-initgues=$4
-python run_cr_rejection.py 20 -dir1 /Users/nmiles/hst_cosmic_rays/analyzing_cr_rejection/"$exptime1"_clean/ -dir2 /Users/nmiles/hst_cosmic_rays/analyzing_cr_rejection/"$exptime2"_clean/ -crsigmas $crsigmas -initgues $initgues
